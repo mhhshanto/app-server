97cce152154955d135f2550861ae1ee476066109
shanto <hhassibul@gmail.com>
2024-03-29 19:19:51 +0600

    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 // middlewear 
 app.use(cors());
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Worldd!')
 })
 
 // mongodb confiq here
